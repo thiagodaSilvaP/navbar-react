e462916b49b854c3afc2c6ed53b513e19c614ea7
Thiago Pereira da Silva <thi.pereira1704@gmal.com>
2022-02-26 20:53:26 -0300

    blah

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+
+import { SideBarPage } from './pages/SideBarPage';
+
 import './App.css';
+import { HeaderPage } from './pages/Header';
+
+
 
 function App() {
+  const [isNavBar, setIsNavBar] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <HeaderPage isNavBar={isNavBar} setIsNavBar={setIsNavBar} />
+      <SideBarPage isNavBar={isNavBar} setIsNavBar={setIsNavBar} />
     </div>
   );
 }
